cc8b6f3a356efaaeeb028066278864b9669138ed
Maty1ek <galymzakangerov@icloud.com>
2024-04-06 16:28:42 +0500

    now the user can draw only when he presses the mouse

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const mainPaint = document.querySelector('.main_paint')
 const elementWidth = mainPaint.offsetWidth
 const painSize = 15
 
+
 let pixelSize = elementWidth / painSize
 
+let mouseDown = 0
+mainPaint.onmousedown = () => (mouseDown = 1)
+mainPaint.onmouseup = () => (mouseDown = 0)
+
+function changeColor(e) {
+    if(mouseDown) {
+        e.target.style.background = 'red'
+    }
+}
+
 for (let i = 0; i < painSize*painSize; i++) {
-    console.log(pixelSize);
     let pixel = document.createElement('div')
     pixel.setAttribute('style', `width:${pixelSize}px; height: ${pixelSize}px;`)
     mainPaint.appendChild(pixel)
-    pixel.addEventListener('mousedown', (e) => pixel.style.background = 'red')
+    pixel.addEventListener('mouseover', changeColor)
+    pixel.addEventListener('mousedown', changeColor)
 }
