00a1a04057b0d52376d86bda56b0782ca74bb696
Maty1ek <galymzakangerov@icloud.com>
2024-04-06 13:56:06 +0500

    Created a visual decoration of the project and main square to drawing

---
+++ practice-conspects/conspect.js
@@ -0,0 +1,4 @@
+// выводит ширину или высоту элемента с учетом границ
+const mainPaint = document.querySelector('.main_paint')
+console.log(mainPaint.offsetWidth)
+console.log(mainPaint.offsetHeight)
